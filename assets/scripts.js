da09e95ee631ce46f1806989711bff1f7c324cbb
chessoft <chessoft@hotmail.com>
2023-08-11 22:31:42 +0200

    optimisation images_1

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,15 +1,59 @@
-$(document).ready(function() {
-    $('.gallery').mauGallery({
-        columns: {
-            xs: 1,
-            sm: 2,
-            md: 3,
-            lg: 3,
-            xl: 3
-        },
-        lightBox: true,
-        lightboxId: 'myAwesomeLightbox',
-        showTags: true,
-        tagsPosition: 'top'
-    });
+$(document).ready(function () {
+  $(".gallery").mauGallery({
+    columns: {
+      xs: 1,
+      sm: 2,
+      md: 3,
+      lg: 3,
+      xl: 3,
+    },
+    lightBox: true,
+    lightboxId: "myAwesomeLightbox",
+    showTags: true,
+    tagsPosition: "top",
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function () {
+  const BREAKPOINTS = [
+    { device: "desktop", query: "(min-width: 1001px) {size}px" },
+    {
+      device: "tablet",
+      query: "(min-width: 651px) and (max-width: 1000px) {size}px",
+    },
+    { device: "mobile", query: "(max-width: 650px) {size}px" },
+  ];
+
+  document.querySelectorAll(".adaptive-picture").forEach((picture) => {
+    const sizes = picture.getAttribute("data-sizes").split(",");
+    const srcsetTemplate = picture.getAttribute("data-srcset");
+
+    const imgData = picture.getAttribute("data-img").split(",");
+    const imgClass = imgData[0];
+    const imgUrl = imgData[1];
+    const imgAlt = imgData[2];
+    const imgWidth = imgData[3];
+    const imgHeight = imgData[4];
+
+    let source = document.createElement("source");
+    source.type = "image/webp";
+    source.srcset = BREAKPOINTS.map(
+      (b, i) =>
+        srcsetTemplate.replace("{device}", b.device) + " " + sizes[i] + "w"
+    ).join(", ");
+    source.sizes = BREAKPOINTS.map((b, i) =>
+      b.query.replace("{size}", sizes[i])
+    ).join(", ");
+
+    picture.appendChild(source);
+
+    let img = document.createElement("img");
+    img.src = imgUrl;
+    img.alt = imgAlt;
+    img.width = imgWidth;
+    img.height = imgHeight;
+    img.className = imgClass.trim() ? imgClass : "";
+
+    picture.appendChild(img);
+  });
 });
